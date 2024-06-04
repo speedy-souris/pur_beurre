a3a78bcc77b2941ff13fd683a48b208dddf6b05c
Pascal MACCHI <pascal.macchi@gmail.com>
2024-06-04 07:30:55 +0200

    modify get_better_nutriscore_list function ban 'POST' request during 'GET' request

--- healthy_food/food_selection/views.py
+++ healthy_food/food_selection/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from food_selection.forms import SearchNewFood, ContactUsForm
 from food_selection.models import Product, Category
 
@@ -18,16 +18,16 @@ def recorded(request):
         if form.is_valid():
             category_1 = form.cleaned_data['product']
     else:
-        form = SearchNewFood()
+        form = get_object_or_404(SearchNewFood)
     nutriscore_1 = get_better_nutriscore_list('C')
     print(f'nutriscore recherché = {nutriscore_1}')
     context = ''
     for n in nutriscore_1:
         print(n)
         products = Product.objects.all().filter(nutriscore__contains=n, categories__name=category_1)
+        print(f'products = {products}')
         context = {'nom': 'produit recherché', 'products': products, 'form': form}
         print(f'context = {context}')
-        print(f'products = {products}')
     return render(request,
                   'food_selection/recorded_product.html', context)
 
@@ -54,26 +54,7 @@ def disclaimer(request):
 
 def get_better_nutriscore_list(nutriscore):
     """replace a bad nutriscore with the list of better nutriscores (e.g. nutriscore D replaced with list A , B, C)"""
-    # nutriscore product B
-    nutriscore_a = ['A']
-    # nutriscore product C
-    nutriscore_b = list(nutriscore_a)
-    nutriscore_b.append('B')
-    # nutriscore product D
-    nutriscore_c = list(nutriscore_b)
-    nutriscore_c.append('C')
-    # nutriscore product E
-    nutriscore_d = list(nutriscore_c)
-    nutriscore_d.append('D')
-    if nutriscore == 'B':
-        nutriscores = nutriscore_a
-    elif nutriscore == 'C':
-        nutriscores = nutriscore_b
-    elif nutriscore == 'D':
-        nutriscores = nutriscore_c
-    elif nutriscore == 'E':
-        nutriscores = nutriscore_d
-    else:
-        nutriscores = nutriscore
-    return nutriscores
+    nutriscores = ['A', 'B', 'C', 'D', 'E']
+    nutriscores_finded = nutriscores[:nutriscores.index(nutriscore)]
+    return nutriscores_finded
 
