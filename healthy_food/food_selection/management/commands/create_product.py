70401e3249992a5cbdbb1fb84ef45bd42276480c
Pascal MACCHI <pascal.macchi@gmail.com>
2024-09-20 16:06:00 +0200

    in the create_product module in the create_relation_categories_products method rename product to product_as_json, rename name_product to product_as_object in the models module add product_id

--- healthy_food/food_selection/management/commands/create_product.py
+++ healthy_food/food_selection/management/commands/create_product.py
@@ -9,53 +9,60 @@ class Command(BaseCommand):
 
     @staticmethod
     def create_categories(all_categories):
+        categories = set()
         for category in all_categories:
             try:
                 Category.objects.get(name=category)
             except Category.DoesNotExist:
-                Category.objects.bulk_create([Category(name=category)])
+                categories.add(Category(name=category))
             else:
                 # self.stdout.write('la categorie existe deja')
                 pass
+        Category.objects.bulk_create(categories)
 
     @staticmethod
     def create_products(products):
+        products_lst = set()
         for product in products:
             if len(product['nutriscore']) > 1:  # exempple nutriscore = 'NOT-APPLICABLE'
                 continue
             try:
-                Product.objects.get(name=product['name'])
+                Product.objects.get(product_id=product['product_id'])
             except Product.DoesNotExist:
-                Product.objects.bulk_create([Product(name=product['name'],
-                                                     nutriscore=product['nutriscore'],
-                                                     url=product['url'])])
+                products_lst.add(Product(name=product ['name'],
+                                         product_id=product['product_id'],
+                                         nutriscore=product['nutriscore'],
+                                         url=product['url']))
             else:
                 # self.stdout.write('le produit existe deja')
                 pass
+        Product.objects.bulk_create(products_lst)
 
     @staticmethod
     def create_relation_categories_products(products):
-        for product in products:
+        for product_as_json in products:
             try:
-                name_product = Product.objects.get(name=product['name'])
+                product_as_object = Product.objects.get(product_id=product_as_json['product_id'])
             except Product.DoesNotExist:
                 # print("Le produit n'existe pas")
                 pass
             else:
-                for category in product['categories']:
+                for category in product_as_json['categories']:
                     name_category = Category.objects.get(name=category)
-                    name_product.categories.add(name_category)
+                    print(f'produit = {product_as_json}')
+                    product_as_object.categories.add(name_category)
 
     def handle(self, *args, **options):
+        image = ''
         products = []
-        categories_list = ['Pâtes alimentaires de céréales', 'Boissons',
-                           'Mélanges de légumes frais', 'fruits secs', 'poissons',
-                           'biscottes', 'patisseries', 'fromages', 'charcuteries', 'confitures']
+        categories_list = {'Pâtes alimentaires de céréales', 'Boissons'}
+                           #'Mélanges de légumes frais', 'fruits secs', 'poissons',
+                           #'biscottes', 'patisseries', 'fromages', 'charcuteries', 'confitures'}
+        counter = 1
         # URL produit openfoodfacts
         for category in categories_list:
-            url = f"https://fr.openfoodfacts.org/api/v2/search?categories_tags_fr={category}"\
-                    "&fields=product_name_fr,nutriscore_grade,categories_tags_fr,url,image_url&page=1&page_size=100"
-            # product infos
+            url = f"https://fr.openfoodfacts.org/api/v1/search?categories_tags_fr={category}"\
+                   "&fields=code,product_name_fr,nutriscore_grade,categories_tags_fr,url,image_url&page=1&page_size=100"            # product infos
             product_infos = requests.get(url)
             print(product_infos.status_code)
             data = json.loads(product_infos.text)
@@ -68,21 +75,30 @@ class Command(BaseCommand):
                 else:
                     name = product['product_name_fr']
                 print()
+                print(counter)
                 print(f'name = {name}')
                 print()
+                print(product['code'])
+                # image_product_infos = requests.get(product['image_url'])
+                # if  image_product_infos.status_code == 200:
+                #     f = open("food_selection/static/food_selection/images/image.jpg", "wb")
+                #     f.write(image_product_infos.content)
+                #     f.close()
                 product3 = {
                             'name': name,
                             'categories': product['categories_tags_fr'],
                             'nutriscore': product['nutriscore_grade'].upper(),
                             'url': product['url'],
-                            'image': product['image_url']
+                            'product_id': product['code']
+                            # 'image': product['image_url']
                             }
                 products.append(product3)
                 print(f'produit final : {products[-1]}')
-                all_categories = [categories for categories in products for categories in categories['categories']]
-                # create categories
-                self.create_categories(all_categories)
-                # create products
-                self.create_products(products)
-                # add relation categories with products
-                self.create_relation_categories_products(products)
+                counter += 1
+        all_categories = [categories for categories in products for categories in categories['categories']]
+        # create categories
+        self.create_categories(all_categories)
+        # create products
+        self.create_products(products)
+        # add relation categories with products
+        self.create_relation_categories_products(products)
--- healthy_food/food_selection/models.py
+++ healthy_food/food_selection/models.py
@@ -4,7 +4,7 @@ from django.db.models import Model
 
 
 class Category(models.Model):
-    name = models.fields.CharField(max_length=10000)
+    name = models.fields.CharField(max_length=10000, primary_key=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -13,8 +13,11 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.fields.CharField(max_length=200)
     nutriscore = models.fields.CharField(max_length=1)
+    product_id = models.fields.CharField(max_length=25, primary_key=True)
     categories = models.ManyToManyField(Category)
     url = models.fields.URLField(max_length=500)
+    image = models.fields.files.ImageField(upload_to='images')
+    # image = models.fields.URLField(max_length=500)
 
     def __str__(self):
         return f'{self.name}'
