f16c054c39bfe4387664eb83ea9fdbd60a343f8e
Pascal MACCHI <pascal.macchi@gmail.com>
2024-09-21 10:16:25 +0200

    rename all variables in the create_product module in django's commands module

--- healthy_food/food_selection/management/commands/create_product.py
+++ healthy_food/food_selection/management/commands/create_product.py
@@ -5,100 +5,103 @@ import requests
 
 
 class Command(BaseCommand):
-    help = 'registering products from the openfoodfact site to the database '
+    help = 'Create product and categories for registering products from the openfoodfact site to the database '
 
     @staticmethod
-    def create_categories(all_categories):
-        categories = set()
-        for category in all_categories:
+    def create_categories(categories_as_final_json_list):
+        categories_object_list = set()
+        for category_as_json in categories_as_final_json_list:
             try:
-                Category.objects.get(name=category)
+                Category.objects.get(name=category_as_json)
             except Category.DoesNotExist:
-                categories.add(Category(name=category))
+                categories_object_list.add(Category(name=category_as_json))
             else:
                 # self.stdout.write('la categorie existe deja')
                 pass
-        Category.objects.bulk_create(categories)
+        Category.objects.bulk_create(categories_object_list)
 
     @staticmethod
-    def create_products(products):
-        products_lst = set()
-        for product in products:
-            if len(product['nutriscore']) > 1:  # exempple nutriscore = 'NOT-APPLICABLE'
+    def create_products(products_final_json_list):
+        products_object_list = set()
+        for product_as_json in products_final_json_list:
+            if len(product_as_json['nutriscore']) > 1:  # exempple nutriscore = 'NOT-APPLICABLE'
                 continue
             try:
-                Product.objects.get(product_id=product['product_id'])
+                Product.objects.get(product_id=product_as_json['product_id'])
             except Product.DoesNotExist:
-                products_lst.add(Product(name=product ['name'],
-                                         product_id=product['product_id'],
-                                         nutriscore=product['nutriscore'],
-                                         url=product['url']))
+                products_object_list.add(Product(name=product_as_json['name'],
+                                         product_id=product_as_json['product_id'],
+                                         nutriscore=product_as_json['nutriscore'],
+                                         url=product_as_json['url']))
             else:
                 # self.stdout.write('le produit existe deja')
                 pass
-        Product.objects.bulk_create(products_lst)
+        Product.objects.bulk_create(products_object_list)
 
     @staticmethod
-    def create_relation_categories_products(products):
-        for product_as_json in products:
+    def create_relation_categories_products(products_final_json_list):
+        for product_as_json in products_final_json_list:
             try:
                 product_as_object = Product.objects.get(product_id=product_as_json['product_id'])
             except Product.DoesNotExist:
                 # print("Le produit n'existe pas")
                 pass
             else:
-                for category in product_as_json['categories']:
-                    name_category = Category.objects.get(name=category)
+                for category_as_json in product_as_json['categories']:
+                    category_as_object = Category.objects.get(name=category_as_json)
                     print(f'produit = {product_as_json}')
-                    product_as_object.categories.add(name_category)
+                    product_as_object.categories.add(category_as_object)
 
     def handle(self, *args, **options):
         image = ''
-        products = []
-        categories_list = {'Pâtes alimentaires de céréales', 'Boissons'}
-                           #'Mélanges de légumes frais', 'fruits secs', 'poissons',
-                           #'biscottes', 'patisseries', 'fromages', 'charcuteries', 'confitures'}
+        products_final_json_list = []
+        categories_object_list = ['Pâtes alimentaires de céréales', 'Boissons',
+                           'Mélanges de légumes frais', 'fruits secs', 'poissons',
+                           'biscottes', 'patisseries', 'fromages', 'charcuteries', 'confitures']
         counter = 1
-        # URL produit openfoodfacts
-        for category in categories_list:
-            url = f"https://fr.openfoodfacts.org/api/v1/search?categories_tags_fr={category}"\
-                   "&fields=code,product_name_fr,nutriscore_grade,categories_tags_fr,url,image_url&page=1&page_size=100"            # product infos
+        # URL product openfoodfacts
+        for category_as_element in categories_object_list:
+            url = f"https://fr.openfoodfacts.org/api/v1/search?categories_tags_fr={category_as_element}"\
+                   "&fields=code,product_name_fr,nutriscore_grade,categories_tags_fr,url,image_url&page=1&page_size=100"
+            # product infos
             product_infos = requests.get(url)
             print(product_infos.status_code)
-            data = json.loads(product_infos.text)
-            for product in data['products']:
+            data_as_json = json.loads(product_infos.text)
+            for product_as_object in data_as_json['products']:
                 try:
-                    if product['product_name_fr'] == '':
+                    if product_as_object['product_name_fr'] == '':
                         continue
                 except KeyError:
                     continue
                 else:
-                    name = product['product_name_fr']
+                    name_object = product_as_object['product_name_fr']
                 print()
                 print(counter)
-                print(f'name = {name}')
+                print(f'name = {name_object}')
                 print()
-                print(product['code'])
+                print(product_as_object['code'])
                 # image_product_infos = requests.get(product['image_url'])
                 # if  image_product_infos.status_code == 200:
                 #     f = open("food_selection/static/food_selection/images/image.jpg", "wb")
                 #     f.write(image_product_infos.content)
                 #     f.close()
-                product3 = {
-                            'name': name,
-                            'categories': product['categories_tags_fr'],
-                            'nutriscore': product['nutriscore_grade'].upper(),
-                            'url': product['url'],
-                            'product_id': product['code']
+                create_final_product_object = {
+                            'name': name_object,
+                            'categories': product_as_object['categories_tags_fr'],
+                            'nutriscore': product_as_object['nutriscore_grade'].upper(),
+                            'url': product_as_object['url'],
+                            'product_id': product_as_object['code']
                             # 'image': product['image_url']
                             }
-                products.append(product3)
-                print(f'produit final : {products[-1]}')
+                products_final_json_list.append(create_final_product_object)
+                print(f'produit final : {products_final_json_list[-1]}')
                 counter += 1
-        all_categories = [categories for categories in products for categories in categories['categories']]
+        categories_as_final_json_list = [
+                                    categories_as_element for categories_as_json in products_final_json_list
+                                        for categories_as_element in categories_as_json['categories']]
         # create categories
-        self.create_categories(all_categories)
+        self.create_categories(categories_as_final_json_list)
         # create products
-        self.create_products(products)
+        self.create_products(products_final_json_list)
         # add relation categories with products
-        self.create_relation_categories_products(products)
+        self.create_relation_categories_products(products_final_json_list)
