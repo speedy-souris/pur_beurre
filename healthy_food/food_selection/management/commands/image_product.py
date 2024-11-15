5ece2fad0dbc70efcfa299ed6ff7547c922c508f
Pascal MACCHI <pascal.macchi@gmail.com>
2024-11-15 18:46:59 +0100

    image_product command modification and enhancement added the following functions test if query empty test if new directory for product images is created test if image file exists and is not empty

--- healthy_food/food_selection/management/commands/image_product.py
+++ healthy_food/food_selection/management/commands/image_product.py
@@ -1,7 +1,8 @@
 
 from django.core.management.base import BaseCommand
-from food_selection.models import Product, Category
+from food_selection.models import Product
 import requests
+import os
 
 class Command(BaseCommand):
     help = 'add images in database'
@@ -9,18 +10,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         products = Product.objects.all()
         counter = 0
+        images_folder_path = "food_selection/static/food_selection/images/image_product"
         for product in products:
+            counter += 1
+            if counter % int((len(products) / 50)) == 0:
+                print(f"compteur : {counter}/{len(products)}")
             image_product_id = product.product_id
+            image_file_path = f"{images_folder_path}/{image_product_id}.jpg"
+            if not os.path.exists(images_folder_path):
+                os.makedirs(images_folder_path)
+            if os.path.exists(image_file_path):
+                continue
             try:
                 response = requests.get(product.image_url)
-            except Exception as inst:
-                print(type(inst))
+            except requests.exceptions.MissingSchema:
                 continue
-            if  response.status_code == 200:
-                f =      open(f"food_selection/static/food_selection/images/image_product      {image_product_id}.jpg", "wb")
-                f.write(response.content)
-                f.close()
-            counter += 1
-            if counter % int((len(products)/50)) == 0 :
-              print(f"compteur : {counter}/{len(products)}")
-
+            if not bool(response.content):
+                continue
+            else:
+                if  response.status_code == 200:
+                    f = open(image_file_path, "wb")
+                    f.write(response.content)
+                    f.close()
