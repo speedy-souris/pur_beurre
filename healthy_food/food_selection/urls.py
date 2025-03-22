1038a15bc4023656b234a364947a894e4990214c
Pascal Macchi <pascal.macchi@gmail.com>
2025-03-22 16:05:47 +0100

    in the url.py module path saved_products added in products template modification of the size of the saved products display frame

--- healthy_food/food_selection/urls.py
+++ healthy_food/food_selection/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('contact-us/', views.contact, name='contact'),
     path('disclaimer/', views.disclaimer, name='disclaimer'),
     path('test_form/', views.TestFormView.as_view(), name='test-form'),
+    path("save_products/", views.SaveProductFormView.as_view(), name="save_products"),
     path("saved_products_list/", SavedProductsListView.as_view(), name="saved-list"),
 ]
