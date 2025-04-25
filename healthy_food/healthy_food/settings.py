da3f3be314ceec9101aa74a6cf9c7119857ce928
Pascal Macchi <pascal.macchi@gmail.com>
2025-04-25 10:24:52 +0200

    in the healthy_food module  1)in the settings.py file  in the templates section  modification of the DIRS[] block  addition BASE_DIRS / 'food_selection' / 'templates'  (indication of the template path for django)  2)in the urls.py file  in the urlpatterns = [] block  path(' ', include('food_selection.urls'))  to  path('food_selection', include('food_selection.urls'))

--- healthy_food/healthy_food/settings.py
+++ healthy_food/healthy_food/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'healthy_food.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # new code that points to the location of the templates
+            BASE_DIR / 'food_selection' / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
