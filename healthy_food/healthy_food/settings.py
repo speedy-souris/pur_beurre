cd04c39d30daf791e0ec7f1a57d9eef747f1e042
Pascal Macchi <pascal.macchi@gmail.com>
2025-11-19 19:53:07 +0100

    creation of authentication application creation of management script for users and super users => managers creation of models module creation of views module creation of test module

--- healthy_food/healthy_food/settings.py
+++ healthy_food/healthy_food/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentification',
     'food_selection',
 ]
 
@@ -134,6 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 MEDIA_URL = '/images/'
 
+# authenfication settings
+AUTH_USER_MODEL = 'authentification.User'
 # ==================================================================
 # CUSTOM PROJECT SETTINGS
 # ==================================================================
