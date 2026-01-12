84e7df8d6fa1e05c7450b06b4536a80c155bb6be
pascal <pascal.macchi@gmail.com>
2026-01-12 23:20:19 +0100

    product template deletion of image directory

--- healthy_food/healthy_food/settings/base.py
+++ healthy_food/healthy_food/settings/base.py
@@ -83,7 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
 
