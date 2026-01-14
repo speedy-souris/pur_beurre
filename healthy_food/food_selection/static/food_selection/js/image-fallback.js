29b4a870f57b9b38db0a73d05daa4f241859bc49
pascal <pascal.macchi@gmail.com>
2026-01-14 14:27:48 +0100

    In food_selection/js modification of the image-fallback.js file addition of the {once: true} option

--- healthy_food/food_selection/static/food_selection/js/image-fallback.js
+++ healthy_food/food_selection/static/food_selection/js/image-fallback.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("img[data-fallback]").forEach(img => {
     img.addEventListener("error", () => {
       img.src = img.dataset.fallback;
-    });
+    }, {once: true});
   });
 });
