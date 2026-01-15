2e20109631f38359e0bf25eb207b216331bbdd17
pascal <pascal.macchi@gmail.com>
2026-01-15 10:21:57 +0100

    in settings change from BASE_DIR  = Path(__file__).resolve().parent.parent.parent to Path(__file__).resolve().parent.parent

--- healthy_food/healthy_food/settings/base.py
+++ healthy_food/healthy_food/settings/base.py
@@ -7,7 +7,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get('DEBUG') == 'True'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
