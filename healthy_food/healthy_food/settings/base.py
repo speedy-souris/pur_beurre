db5d8e9ed6974b53be378979a42f3e2e11f37fd8
pascal <pascal.macchi@gmail.com>
2026-01-15 22:01:01 +0100

    in settings.base addition parent argument of the BASE_DIR

--- healthy_food/healthy_food/settings/base.py
+++ healthy_food/healthy_food/settings/base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 SECRET_KEY = os.environ.get('SECRET_KEY')
