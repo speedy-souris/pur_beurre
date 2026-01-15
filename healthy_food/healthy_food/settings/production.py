155850df7e41eb172c943ce91626cff5a3bd804e
pascal <pascal.macchi@gmail.com>
2026-01-15 11:27:00 +0100

    in the saved_products template change the location of the default.jpg image in data-fallback images/image_product to assets/img

--- healthy_food/healthy_food/settings/production.py
+++ healthy_food/healthy_food/settings/production.py
@@ -18,8 +18,10 @@ if db_from_env:
 #         conn_max_age=600
 #     )
 # }
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # WhiteNoise optimization for production (static file compression)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Production-specific security
 SECURE_SSL_REDIRECT = True
